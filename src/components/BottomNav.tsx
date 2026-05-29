ad93857fe3846db80913aca6b7303908de13ccb0
Abdulhadi <abdulmuminhadi76@gmail.com>
2026-05-29 23:31:17 +0000

    Updated project files

---
+++ src/components/BottomNav.tsx
@@ -0,0 +1,3 @@
+export default function BottomNav() {
+  return <nav className="fixed bottom-0 w-full bg-[#1A1A1A] h-16" />
+}
---
+++ src/screens/ShopScreen.tsx
@@ -0,0 +1,3 @@
+export default function ShopScreen() {
+  return <div className="p-4 text-white">Shop</div>
+}
